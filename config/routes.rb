9761c587f1189140294c47e75cfd9dd98130355f
Tako <takoforme@gmail.com>
2020-08-12 16:15:56 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_scope :user do
+    get 'new_name', to: 'devise/registrations#new_name', as: 'new_name'
+    patch 'create_name', to: 'users/registrations#create_name', as: 'create_name'
+  end
   get 'users/:id' => 'users#show'
   root 'boards#index'
   resources :boards
